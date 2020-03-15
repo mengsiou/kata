c143ddbb60946e5dec95a82ed54d67d7be21d95a
MilesWu <speater306@gmail.com>
2020-03-15 22:39:12 +0800

    Initial commit

---
+++ Kata/StringAverage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    public class StringAverage
+    {
+        public static string AverageString(string str)
+        {
+            if (String.IsNullOrEmpty(str)) return "n/a";
+            string[] numText = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+            string[] numList = str.Split(' ');
+
+            int sum = 0;
+            foreach (string num in numList)
+            {
+                int numValue = Array.IndexOf(numText, num);
+                if (numValue < 0) return "n/a";
+                sum += numValue;
+            }
+
+            return numText[(int)Math.Floor((double)sum / (double)numList.Length)];
+        }
+    }
+}
---
+++ KataTests/StringAverageTests.cs
@@ -0,0 +1,27 @@
+﻿using Kata;
+using NUnit.Framework;
+
+namespace KataTests
+{
+    public class StringAverageTests
+    {
+        [Test]
+        public void EmptyInput_ReturnNA() 
+        {
+            Assert.AreEqual("n/a", StringAverage.AverageString(""));
+            Assert.AreEqual("n/a", StringAverage.AverageString(null));
+            Assert.AreEqual("n/a", StringAverage.AverageString("1234"));
+            Assert.AreEqual("n/a", StringAverage.AverageString("ten nine"));
+        }
+        [Test]
+        public void ValidInput_ReturnNumString()
+        {
+            Assert.AreEqual("four", StringAverage.AverageString("zero nine five two"));
+            Assert.AreEqual("three", StringAverage.AverageString("four six two three"));
+            Assert.AreEqual("three", StringAverage.AverageString("one two three four five"));
+            Assert.AreEqual("four", StringAverage.AverageString("five four"));
+            Assert.AreEqual("zero", StringAverage.AverageString("zero zero zero zero zero"));
+            Assert.AreEqual("two", StringAverage.AverageString("one one eight one"));
+        }
+    }
+}
